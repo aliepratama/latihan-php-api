084de47612d868242ba23265131b5139f175977d
aliepratama <aliepratama2@gmail.com>
2023-12-15 00:02:42 +0700

    feat: make put and delete api

--- api/index.php
+++ api/index.php
@@ -18,20 +18,39 @@ header("Access-Control-Allow-Origin: *");
 $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'GET':
-        try{
-            $data = $db->fetchAll()->getResult();
-            http_response_code(200);
-            echo json_encode([
-                'message' => 'Todo berhasil dimuat!',
-                'data' => $data
-            ]);
-        }
-        catch(Exception $e){
-            http_response_code(500);
-            echo json_encode([
-                'message' => 'Todo gagal dimuat!',
-                'reason' => $e->getMessage()
-            ]);
+        $getId = $_GET['id'] ?? null;
+        if($getId){
+            try{
+                $data = $db->findBy('id', $getId)->getResult();
+                http_response_code(200);
+                echo json_encode([
+                    'message' => 'Todo berhasil dimuat!',
+                    'data' => $data
+                ]);
+            }
+            catch(Exception $e){
+                http_response_code(500);
+                echo json_encode([
+                    'message' => 'Todo gagal dimuat!',
+                    'reason' => $e->getMessage()
+                ]);
+            }
+        } else {
+            try{
+                $data = $db->fetchAll()->getResult();
+                http_response_code(200);
+                echo json_encode([
+                    'message' => 'Todo berhasil dimuat!',
+                    'data' => $data
+                ]);
+            }
+            catch(Exception $e){
+                http_response_code(500);
+                echo json_encode([
+                    'message' => 'Todo gagal dimuat!',
+                    'reason' => $e->getMessage()
+                ]);
+            }
         }
     break;
     case 'POST':
@@ -59,6 +78,68 @@ switch ($method) {
             echo json_encode([
                 'message' => 'Todo gagal ditambahkan!',
                 'reason' => $e->getMessage()
+            ]);
+        }
+    break;
+    case 'PUT':
+        $getId = $_GET['id'] ?? null;
+        if($getId){
+            try{
+                $data = json_decode(file_get_contents('php://input'), true);
+                $title = $data['title'];
+                $description = $data['description'];
+                $priority = $data['priority'];
+
+                $newTodo = [
+                    'title' => $title,
+                    'description' => $description,
+                    'priority' => $priority,
+                ];
+
+                $data = $db->update($getId, $newTodo);
+                http_response_code(200);
+                echo json_encode([
+                    'message' => 'Todo berhasil diubah!',
+                    'data' => $data
+                ]);
+            }
+            catch(Exception $e){
+                http_response_code(500);
+                echo json_encode([
+                    'message' => 'Todo gagal diubah!',
+                    'reason' => $e->getMessage()
+                ]);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode([
+                'message' => 'Todo gagal diubah!',
+                'reason' => 'Tidak menyertakan params pada endpoint'
+            ]);
+        }
+    break;
+    case 'DELETE':
+        $getId = $_GET['id'] ?? null;
+        if($getId){
+            try{
+                $db->delete($getId);
+                http_response_code(200);
+                echo json_encode([
+                    'message' => 'Todo berhasil dihapus!',
+                ]);
+            }
+            catch(Exception $e){
+                http_response_code(500);
+                echo json_encode([
+                    'message' => 'Todo gagal dihapus!',
+                    'reason' => $e->getMessage()
+                ]);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode([
+                'message' => 'Todo gagal diubah!',
+                'reason' => 'Tidak menyertakan params pada endpoint'
             ]);
         }
     break;
